ca854f9bd732172e625c0f8c75d9ad183632061d
Jeff Oliver <jcoliver@email.arizona.edu>
2026-03-25 14:51:36 -0700

    Update entry distribution code and add label for FiLa

--- prepare-evaluation-mult.R
+++ prepare-evaluation-mult.R
@@ -8,6 +8,11 @@ library(tidyverse)
 library(rmarkdown)
 
 eval_year <- "2026"
+# Minimum number of evaluations for each entry. Will be used with the number of 
+# entries (i.e. number of rows in data/YYYY/responses.csv) and the number of 
+# judges (i.e. number of rows in data/YYYY/judges.csv) to determine 
+# assignments. About 95% confident the math works.
+min_evals <- 3
 
 # This will create
 # 1. Sheet for scoring entries, (or part of it) with FiLa as column heading, 
@@ -58,7 +63,7 @@ uniq_responses <- responses %>%
 # }
 # table(rand_assign)
 
-# 1. Sheet for evaluations
+# 1. Sheet for overall evaluations; includes all FiLa combinations
 initials <- paste0(substr(x = uniq_responses$`First Name`, start = 1, stop = 2),
                    substr(x = uniq_responses$`Last Name`, start = 1, stop = 2))
 eval_sheet <- data.frame(matrix(nrow = 1, ncol = length(initials)))
@@ -76,14 +81,55 @@ readr::write_csv(x = eval_sheet, file = paste0("output/", eval_year, "/evaluatio
 # assigned to that judge
 judges <- read.csv(file = paste0("data/", eval_year, "/judges.csv"))
 
-# TODO: Update judges csv (or other mode?) to assign specific entries to each 
-# judge
-# TODO: Update evaluations.csv output (above) to create separate CSV for each 
-# judge
+# Number of evaluations for each judge to perform
+num_evals <- ceiling(min_evals * nrow(uniq_responses) / nrow(judges))
+
+# Matrix of evaluation assignments
+eval_assignments <- matrix(data = 1:nrow(uniq_responses), 
+                           nrow = nrow(judges), 
+                           ncol = num_evals, 
+                           byrow = TRUE)
+rownames(eval_assignments) <- judges$name
+# This is fine, but *rows* are not unique (that is, three judges evaluate the 
+# same 16 entries). Add validator and iterate shuffling until each row is 
+# unique
+valid <- FALSE
+num_attempts <- 0
+set.seed(20260325)
+while (!valid & num_attempts < 100) {
+  eval_assignments <- apply(X = eval_assignments,
+                            MARGIN = 2,
+                            FUN = function(x) {
+                              return(sample(x))
+                            })
+  # Kudos to Claude for this validation code
+  valid <- all(apply(X = eval_assignments, 
+                     MARGIN = 1, 
+                     FUN = function(row) {
+                       return(length(row) == length(unique(row)))
+                     }))
+  num_attempts <- num_attempts + 1
+}
+if (valid) {
+  message("Assignments complete after ", num_attempts, " attempts.")
+} else {
+  warning("Could not assign evaluations after ", num_attempts, " attempts.")
+}
+# Ideally things worked, and we can sort each row for easier viewing
+# I don't love that I have to transpose the apply output :(
+eval_assignments <- t(apply(X = eval_assignments,
+                          MARGIN = 1,
+                          FUN = function(i) {
+                            return(sort(i))
+                          }))
+
+# TODO: Create evaluations CSV for each judge
 
 for (j in 1:nrow(judges)) {
   judge_name <- judges$name[j]
-  responses_for_judge <- uniq_responses[sample(x = 1:nrow(uniq_responses), size = 2), ]
+  # Pull out the rows corresponding to entries assigned to this judge
+  responses_for_judge <- uniq_responses[sort(eval_assignments[j, ]), ]
+  # Make the pdf the judge will use for evaluations
   message("Rendering Entries PDF for ", judge_name)
   rmarkdown::render(input = "Entries-mult.Rmd", 
                     # output_format = "pdf_document",
